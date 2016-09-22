bd751f4e0491e7c067631aa64df1d5f71680e9b1
ggmol <muxiaoyang1991@gmail.com>
2016-09-22 12:48:18 -0400

    feed cell updated

---
+++ social/PostCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostCell.swift
+//  social
+//
+//  Created by Mu on 9/21/16.
+//  Copyright © 2016 Mu. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
